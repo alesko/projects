b15b1a98279499647da7f1582618c4d0329bb315
Alexander Skoglund <alexander.skoglund@gmail.com>
2012-07-06 15:39:26 +0200

    Python program that communicates with the Arduino. Reads forces from a file, sends it to the Arduino and store reposedata in another file. Not yet complete.

--- kalckert/force-servo/src/python/test_experiment.py
+++ kalckert/force-servo/src/python/test_experiment.py
@@ -1,18 +1,59 @@
+#!/usr/bin/python
 
-
+# Import libs
+from Tkinter import *
 import serial
 import time
 #from datetime import date
 from datetime import datetime
 from time import strftime
+#import numpy as np
+import random
 
-fpname = strftime("%Y%m%d-%H%M%S")+'.log'
+# Read force from file and put in array
 
-# Change port name (/dev/tty.usbmodemfd3331) to the appropriate port
+fpname="forces.txt"
+fp = open(fpname,"r");
+A = [];
+c = fp.readline()
+
+while c != '':
+    A.append(float(c));
+    c = fp.readline()
+
+fp.close()
+# Random shuffle the array
+random.shuffle(A)
+
+# Change port name (/dev/ttyXXX) to the appropriate port,
+# XXX is the port name on POSIX system (MacOSX, Linux)
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-#print ser.portstr       # check which port was really used
-#ser.write("hello")      # write a string
+# Change settings in the Arduino
+# Pause duration 1500ms:
+ser.write("P1500;")
+# Number of repetions
+ser.write("N1;")
+# Duration of force task, 3000ms
+ser.write("D3000;")
+# Delay between produced and actuated force: 0 ms
+ser.write("L0;")
+# Number of force elements
+f_len =  len(A);
+cstr = "I" + str(f_len) + ";"
+ser.write(cstr)
+
+# Write the force data to the Arduino
+for i in range(len(A)):
+	cstr = "E" + str(i)+ ";"
+	#ser.write(cstr) 
+	print cstr
+	cstr = "F"+str(A[i]) + ";"
+	#ser.write(cstr) 
+	print cstr
+
+# Set the filename to the curret date and time + .log
+fpname = strftime("%Y%m%d-%H%M%S")+'.log'
 
 # Open file for printing
 fp = open(fpname,"w")
@@ -25,21 +66,7 @@ if (ser.isOpen()):
 	else:
 		dtstr = dt.seconds*1000000+dt.microseconds
 	fp.write(str(dtstr))
-	#ser.write('s')
-	#line = ser.readline(6)
-	#print line
-	#fp.write(str(line))
-	#ser.write("I")
-	#line = ser.readline(6)
-	#fp.write(str(line))
-	#ser.write("A")
-	#line = ser.readline(6)
-	#fp.write(str(line))
 
-	#while ( line != 5 ):
-	#	line = ser.readline(3)
-	#	fp.write(str(line))
-	#	ser.write("S")
 else:
 	print "Error when opening port"
 
